fb03cbd2870c4667272686c302fd4c1751129489
Alina-QA-Engineer <alinaqatests@gmail.com>
2024-11-25 20:38:05 +0200

    Started RegistrationTests

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -1,4 +1,18 @@
 package tests;
 
-public class RegistrationTests {
+import manager.ApplicationManager;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class RegistrationTests extends ApplicationManager {
+
+    @Test
+    public void registrationPositiveTest() {
+
+        new HomePage(getDriver()).clickBtnLoginHeader();
+        new LoginPage(getDriver()).typeLoginForm("testemail@example.com", "password");
+
+    }
+
 }
