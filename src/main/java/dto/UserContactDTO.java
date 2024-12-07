07c4938b64fb56a018cd1d3b99b90520afb38dca
Alina-QA-Engineer <alinaqatests@gmail.com>
2024-12-07 20:51:43 +0200

    Add UserContactDTO, ContactsTests

---
+++ src/main/java/dto/UserContactDTO.java
@@ -0,0 +1,22 @@
+package dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+@Builder
+
+public class UserContactDTO {
+
+    private String name;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private String address;
+    private String description;
+
+}
