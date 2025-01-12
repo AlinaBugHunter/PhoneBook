896b305b54227aec7fbba22a2074c0fe66f3322d
Alina-QA-Engineer <alinaqatests@gmail.com>
2025-01-12 19:52:53 +0200

    Created ResponseMessageDTO

--- src/main/java/utils/BaseAPI.java
+++ src/main/java/utils/BaseAPI.java
@@ -13,5 +13,6 @@ public interface BaseAPI {
     String BASE_URL = "https://contactapp-telran-backend.herokuapp.com";
     String REGISTRATION = "/v1/user/registration/usernamepassword";
     String LOGIN = "/v1/user/login/usernamepassword";
+    String ADD_NEW_CONTACT = "/v1/contacts";
 
 }
