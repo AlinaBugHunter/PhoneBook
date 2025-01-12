0cde7d03894d5c2fb9b35ae6f4d3cffca5701f2a
Alina-QA-Engineer <alinaqatests@gmail.com>
2025-01-12 20:05:09 +0200

    Created AddNewContactTests

---
+++ src/test/java/okhttp/AddNewContactTests.java
@@ -0,0 +1,76 @@
+package okhttp;
+
+import dto.ResponseMessageDTO;
+import dto.TokenDTO;
+import dto.UserContactDTO;
+import dto.UserDTOLombok;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import utils.BaseAPI;
+
+import static utils.RandomUtils.*;
+
+import java.io.IOException;
+
+public class AddNewContactTests implements BaseAPI {
+
+    TokenDTO tokenDTO;
+
+    @BeforeClass
+    public void login() {
+        UserDTOLombok userDTOLombok = UserDTOLombok.builder()
+                .username("testemail@example.com")
+                .password("Password123!")
+                .build();
+        RequestBody requestBody = RequestBody.create(GSON.toJson(userDTOLombok), JSON);
+        Request request = new Request.Builder().url(BASE_URL + LOGIN).post(requestBody).build();
+        try (Response response = OK_HTTP_CLIENT.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                tokenDTO = GSON.fromJson(response.body().string(), TokenDTO.class);
+            } else {
+                System.out.println("Login failed" + response.code());
+            }
+        } catch (IOException e) {
+            System.out.println("Crated Exception login()");
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void addNewContact() {
+
+        UserContactDTO userContactDTO = UserContactDTO.builder()
+                .name("Peter")
+                .lastName("Johnson")
+                .phoneNumber(generatePhone(11))
+                .email(generateEmail(5))
+                .address("Tel Aviv")
+                .description(generateString(15))
+                .build();
+
+        RequestBody requestBody = RequestBody.create(GSON.toJson(userContactDTO), JSON);
+        Request request = new Request.Builder()
+                .url(BASE_URL + ADD_NEW_CONTACT)
+                .addHeader("Authorization", tokenDTO.getToken())
+                .post(requestBody)
+                .build();
+
+        try (Response response = OK_HTTP_CLIENT.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                ResponseMessageDTO responseMessageDTO = GSON.fromJson(response.body().string(), ResponseMessageDTO.class);
+                System.out.println(responseMessageDTO.getMessage());
+                Assert.assertTrue(responseMessageDTO.getMessage().contains("Contact was added!"));
+            } else {
+                Assert.fail("Contact wasn't added" + response.code());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
